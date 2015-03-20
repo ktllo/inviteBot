90031f193708f1c4bca8a8d5bf55fba28ee7bd7d
leolo <leolo@leolo.org>
2015-03-20 10:08:54 +0800

    Add helper for sending message with colours

---
+++ src/org/leolo/ircbot/inviteBot/util/Color.java
@@ -0,0 +1,13 @@
+package org.leolo.ircbot.inviteBot.util;
+
+public class Color {
+	private static final String ESCAPE = "\u0003";
+	
+	public static String color(ColorName foreground){
+		return ESCAPE+foreground.getCode();
+	}
+	
+	public static String color(ColorName foreground,ColorName background){
+		return ESCAPE+background.getCode()+","+foreground.getCode();
+	}
+}
---
+++ src/org/leolo/ircbot/inviteBot/util/ColorName.java
@@ -0,0 +1,28 @@
+package org.leolo.ircbot.inviteBot.util;
+
+public enum ColorName {
+	WHITE(0),
+	BLACK(1),
+	DARK_BLUE(2),
+	DARK_GREEN(3),
+	RED(4),
+	BROWN(5),
+	PURPLE(6),
+	OLIVE(7),
+	YELLOW(8),
+	GREEN(9),
+	TEAL(10),
+	CYAN(11),
+	BLUE(12),
+	MAGENTA(13),
+	DARK_GRAY(14),
+	LIGHT_GRAY(15);
+	
+	private int code;
+	private ColorName(int code){
+		this.code = code;
+	}
+	public int getCode() {
+		return code;
+	}
+}
