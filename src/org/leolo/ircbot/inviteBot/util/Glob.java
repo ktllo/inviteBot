2f11e3f4046cc83febbe4873abdbf6b84b7b5d69
leolo <leolo@leolo.org>
2015-03-20 11:36:58 +0800

    Improved Glob implemention

--- src/org/leolo/ircbot/inviteBot/util/Glob.java
+++ src/org/leolo/ircbot/inviteBot/util/Glob.java
@@ -3,17 +3,38 @@ package org.leolo.ircbot.inviteBot.util;
 public class Glob {
 	
 	/**
-	 * This is a simplified implementation of glob. Which supports 0/1 * only, Or both start/end with *
+	 * This is a simplified implementation of glob. Which supports * only
+	 * 
+	 * The expected time required for 0/1 *, or has 2 * only, while start and
+	 * end with * is O(n). While the cost for other case will be much higher
+	 * 
 	 * @param pattern Pattern to be matched
 	 * @param target Target to be matched
 	 * @return true if matched
 	 */
 	public static boolean match(String pattern,String target){
-		if(pattern.indexOf("*") == -1){
+		int count = 0;
+		for(char c:pattern.toCharArray()){
+			if(c == '*'){
+				count++;
+			}
+		}
+		if(count == 0){
 			return pattern.equalsIgnoreCase(target);
-		}else if(pattern.startsWith("*") && pattern.endsWith("*")){
+		}else if(pattern.startsWith("*") && pattern.endsWith("*") && count == 2){
 			String subpattern=pattern.substring(1,pattern.length()-1);
 			return target.contains(subpattern);
+		}else if(count >= 2){
+			String out = "^";
+		    for(char c: pattern.toCharArray()){
+		        switch(c){
+			        case '*': out += ".*"; break;
+			        case '\\': out += "\\\\"; break;
+			        default: out += c;
+		        }
+		    }
+		    out += '$';
+		    return target.matches(out);
 		}else if(pattern.endsWith("*")){
 			return target.startsWith(pattern.substring(0, pattern.length()-1));
 		}else if(pattern.startsWith("*")){
